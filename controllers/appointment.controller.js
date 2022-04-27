645c035375fd240f453b73d2927d2b7565408876
Coding Expect <dannymccall419@gmail.com>
2022-04-26 19:27:52 -0700

    always editing

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -165,3 +165,4 @@ exports.updateAppointments = (req, res) => {
     })
   }
 }
+
--- controllers/blood.controller.js
+++ controllers/blood.controller.js
@@ -1,8 +1,8 @@
 const Blood = require("../model/blood.model");
-
+const Orders = require("../model/orders.model");
 exports.addBlood = (req, res) => {
   try {
-    let { quantity, selectBloodType } = req.body;
+    let { quantity, selectBloodType, username } = req.body;
     quantity = quantity.toString().trim();
     selectBloodType = selectBloodType.trim();
     let bloodCode = Math.floor(1000 + Math.random() * 9000);
@@ -31,7 +31,7 @@ exports.addBlood = (req, res) => {
           const newBlood = new Blood({
             bloodType: selectBloodType,
             quantity,
-            bloodCode
+            bloodCode,
           });
           newBlood
             .save()
@@ -47,6 +47,70 @@ exports.addBlood = (req, res) => {
                 message: "Something heppened",
               });
             });
+        }
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: "FAILED",
+      message: error.message,
+    });
+  }
+};
+
+exports.orderForBlood = (req, res) => {
+  try {
+    let { quantity, selectBloodGroup, username } = req.body;
+    quantity = quantity.toString().trim();
+    selectBloodGroup = selectBloodGroup.trim();
+    let bloodType = selectBloodGroup;
+    let key;
+    if (quantity == "" || selectBloodGroup == "") {
+      res.json({
+        status: "FAILED",
+        message: "Empty Input Fields",
+      });
+    } else if (isNaN(quantity)) {
+      res.json({
+        status: "FAILED",
+        message: "Quantity must be a number",
+      });
+    } else {
+      Blood.find({ bloodType }).then((blood) => {
+        if (blood.length) {
+          blood.map((value) => {
+            key = value.quantity;
+            if (key <= 0) {
+              res.json({
+                status: "FAILED",
+                message:
+                  "Please we are out of stock at the moment for this particular blood type",
+              });
+            } else if (key < quantity) {
+              res.json({
+                status: "FAILED",
+                message:
+                  "Our Stock is below your order, please reduce your order",
+              });
+            } else {
+              const newQuantity = key - quantity;
+              let bloodCode = blood[0].bloodCode;
+              blood[0].quantity = newQuantity;
+              blood[0].save().then(() => {
+                res.json({
+                  status: "SUCCESS",
+                  message: `Your order has been successfull with blood order ${bloodCode}`,
+                });
+              });
+              console.log(username);
+              const newOrder = new Orders({
+                doctorName: username,
+                bloodType,
+                quantity,
+              });
+              newOrder.save();
+            }
+          });
         }
       });
     }
